d966ac4e5211333807cf67ceae6b8143c8c49838
exe_okan <egeokanunaldi@hotmail.com>
2019-09-25 15:09:10 +0300

    PID kontrol eklendi
    +kod sadeleştirmeleri

--- Mersin Off-Season/src/main/cpp/Robot.cpp
+++ Mersin Off-Season/src/main/cpp/Robot.cpp
@@ -3,70 +3,49 @@
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
+/* KelRot#5655 Deep Space Code for Mersin Off-Season                          */
 /*----------------------------------------------------------------------------*/
 
 #include "Robot.h"
+#include <math.h>
 
 #include <iostream>
 
 #include <frc/smartdashboard/SmartDashboard.h>
-  nt::NetworkTableEntry ntAngle;
-void Robot::RobotInit() {
-  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
-  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
-  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
 
+  void Robot::RobotInit() {
   auto inst=nt::NetworkTableInstance::GetDefault();
   auto table=inst.GetTable("ChickenVision");
   ntAngle=table->GetEntry("HedefAci");
-
-  ultraLeft.SetAutomaticMode(true);
-  ultraRight.SetAutomaticMode(true);
-  gyro.Calibrate();
 }
 
-/**
- * This function is called every robot packet, no matter the mode. Use
- * this for items like diagnostics that you want ran during disabled,
- * autonomous, teleoperated and test.
- *
- * <p> This runs after the mode specific periodic functions, but before
- * LiveWindow and SmartDashboard integrated updating.
- */
 void Robot::RobotPeriodic() {
   kp=prefs->GetDouble("KP",0.00);
   kd=prefs->GetDouble("KD",0.0);
+  ki=prefs->GetDouble("KI",0.0);
+  tempTirmanma=prefs->GetDouble("Tirmanma",0.0); //*tırmanma motorlarına verilecek hızı SmartDashboard'dan almak için
 }
+void Robot::AutonomousInit() {}
 
-/**
- * This autonomous (along with the chooser code above) shows how to select
- * between different autonomous modes using the dashboard. The sendable chooser
- * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
- * remove all of the chooser code and uncomment the GetString line to get the
- * auto name from the text box below the Gyro.
- *
- * You can add additional auto modes by adding additional comparisons to the
- * if-else structure below with additional strings. If using the SendableChooser
- * make sure to add them to the chooser code above as well.
- */
-void Robot::AutonomousInit() {
-  m_autoSelected = m_chooser.GetSelected();
-  // m_autoSelected = SmartDashboard::GetString("Auto Selector",
-  //     kAutoNameDefault);
-  std::cout << "Auto selected: " << m_autoSelected << std::endl;
-
-  if (m_autoSelected == kAutoNameCustom) {
-    // Custom Auto goes here
-  } else {
-    // Default Auto goes here
+void Robot::AutonomousPeriodic() {//! şuan sadece tırmanma denemek için 
+  rd.CurvatureDrive(-js.GetRawAxis(1),js.GetRawAxis(4),js.GetRawButton(5));
+  if(js.GetRawButton(1))
+  {
+    tirmanma1.Set(ControlMode::PercentOutput,tempTirmanma);
+    tirmanma2.Set(ControlMode::PercentOutput,tempTirmanma);
+    std::cout<<tempTirmanma<<std::endl;
   }
- 
-}
-
-void Robot::AutonomousPeriodic() {
-  double distRight=ultraRight.GetRangeMM()/10.0;
-  double distLeft=ultraLeft.GetRangeMM()/10.0;
-  std::cout<<"DistRight:"<<distRight<<" DistLeft:"<<distLeft<<std::endl;
+  else if(js.GetRawButton(2))
+  {
+    tirmanma1.Set(ControlMode::PercentOutput,-tempTirmanma);
+    tirmanma2.Set(ControlMode::PercentOutput,-tempTirmanma);
+    std::cout<<tempTirmanma<<std::endl;
+  }
+  else{
+    tirmanma1.Set(ControlMode::PercentOutput,0.0);
+    tirmanma2.Set(ControlMode::PercentOutput,0.0);
+    
+  }
 }
 
 void Robot::TeleopInit() {
@@ -74,34 +53,63 @@ void Robot::TeleopInit() {
 }
 
 void Robot::TeleopPeriodic() {
-  double angle=ntAngle.GetDouble(777.0);
+  double angle=ntAngle.GetDouble(777.0);//* raspberryden gelen açı değeri,777.0 default değer
   if(angle>700)
   {
     std::cout<<"gormuyor"<<std::endl;
-    angle=lastAngle;
-  
+    goruyor=false; 
   }
   else{
     std::cout << angle << std::endl;
+    goruyor=true;
   }
 
-  if(js.GetRawButtonPressed(1))
-  assist=!assist;
+  if(js.GetRawButtonPressed(1)){
+  assist=!assist; 
+  }
 
-  if(!assist)
+  if(!assist){
   rd.CurvatureDrive(-js.GetRawAxis(1),js.GetRawAxis(4),js.GetRawButton(5));
+  }
   else
   {
     std::cout << "Auto:ON" << std::endl;
-    std::cout << kp <<kd <<std::endl;
-    double rot=(angle/30.0)*kp+(angle/30.0-lastAngle)*kd;
-    lastAngle=(angle/30.0);
+    double rot=0;
+    if(goruyor){
+      double errorP =(angle/30.0);
+      double errorD = (angle/30.0-lastAngle);
+      lastAngle= (angle/30.0);
+      if(abs(angle) <= 8.0 && abs(angle)>0.5){
+        errorI += errorP;
+      }
+      else{//* aralıkta değil ise hesaplama
+        errorI=0;
+      }
+      //* integrali sınırlama
+      if(errorI>  50/ki){
+        errorI = 50/ki;
+      }
+      else if(errorI< -50/ki){
+        errorI = -50/ki;
+      }
+      rot= errorP*kp + errorI*ki + errorD*kd;
+      if(rot>=1.00)
+      {
+        rot= 1.0;
+      }
+      else if(rot<=-1.0)
+      {
+        rot= -1.0;
+      }
+    }
     double speed=-js.GetRawAxis(1);
     rd.ArcadeDrive(speed,rot);
   }
 }
 
-void Robot::TestPeriodic() {}
+void Robot::TestPeriodic() {
+  
+}
 
 #ifndef RUNNING_FRC_TESTS
 int main() { return frc::StartRobot<Robot>(); }
--- Mersin Off-Season/src/main/include/Robot.h
+++ Mersin Off-Season/src/main/include/Robot.h
@@ -3,6 +3,7 @@
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
+/* KelRot#5655 Deep Space Code for Mersin Off-Season                          */
 /*----------------------------------------------------------------------------*/
 
 #pragma once
@@ -16,7 +17,7 @@
 #include <networktables/NetworkTable.h>
 #include <networktables/NetworkTableInstance.h>
 #include <networktables/NetworkTableEntry.h>
-
+#include "ctre/Phoenix.h"
 class Robot : public frc::TimedRobot {
  public:
   void RobotInit() override;
@@ -27,33 +28,27 @@ class Robot : public frc::TimedRobot {
   void TeleopPeriodic() override;
   void TestPeriodic() override;
 
-  
   private:
+  frc::Preferences *prefs=frc::Preferences::GetInstance();
+  nt::NetworkTableEntry ntAngle;
 
-  frc::Preferences *prefs=frc::Preferences::GetInstance();;
-  double kp=0.0,kd=0.0;
-  frc::SendableChooser<std::string> m_chooser;
-  const std::string kAutoNameDefault = "Default";
-  const std::string kAutoNameCustom = "My Auto";
-  std::string m_autoSelected;
-
-  frc::DoubleSolenoid pnueTest{0,1};
   frc::Joystick js{0};
 
-  frc::ADIS16470_IMU gyro{};
-  frc::Ultrasonic ultraLeft{6,8};
-  frc::Ultrasonic ultraRight{7,9};
+  frc::Talon solOn{1};    frc::Talon sagOn{2};
+  frc::Talon solArka{0};  frc::Talon sagArka{3}; 
 
-  frc::Talon solOn{1};  frc::Talon solArka{0}; 
-  frc::Talon sagOn{2};  frc::Talon sagArka{3}; 
-
-  //frc::RobotDrive autoRd{solOn, solArka,sagOn,sagArka}; 
+  VictorSPX tirmanma1{0};
+  VictorSPX tirmanma2{1};
+  double tempTirmanma=0.0; //SmartDashboard verisi için-motorlara verilecek güç 
 
   frc::SpeedControllerGroup left{solOn,solArka};
   frc::SpeedControllerGroup right{sagOn,sagArka};
-  frc::DifferentialDrive rd{left,right};
-  double lastAngle=0;
-  bool assist=false;
-
   
+  frc::DifferentialDrive rd{left,right};
+
+  bool goruyor;
+  double lastAngle=0;
+  double kp=0.0,kd=0.0,ki=0.0;
+  double errorI=0;
+  bool assist=false;
 };
